543350ee34229792a009dd606d5c662258ca03ce
pawan <b419039@iiit-bh.ac.in>
2021-09-14 05:12:56 +0530

    added styles to contact form

--- src/components/name.jsx
+++ src/components/name.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Typist from "react-typist";
 import "./css/name.css";
-import "react-typist/dist/Typist.css"
+import "react-typist/dist/Typist.css";
 export default function Name() {
   return (
     <div className="name">
-      <Typist avgTypingDelay={70} stdTypingDelay={5} startDelay={3000} cursor={{
-  show: true,
-  blink: true,
-  element: '|',
-  hideWhenDone: true,
-  hideWhenDoneDelay: 500,
-} }>
+      <Typist
+        avgTypingDelay={70}
+        stdTypingDelay={5}
+        startDelay={3000}
+        cursor={{
+          show: false,
+        }}
+      >
         <span>
           I am Paw$n
           <Typist.Backspace count={2} delay={200} />
