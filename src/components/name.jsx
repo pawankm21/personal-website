2a2003ed2da2e437353071ad6bd513bc7b86ee67
pawan <b419039@iiit-bh.ac.in>
2021-09-22 21:16:02 +0530

    added board

--- src/components/name.jsx
+++ src/components/name.jsx
@@ -4,26 +4,30 @@ import "./css/name.css";
 import "react-typist/dist/Typist.css";
 export default function Name() {
   return (
-    <div className="name">
-      <Typist
-        avgTypingDelay={50}
-        stdTypingDelay={5}
-        startDelay={2000}
-        cursor={{
-          show: false,
-        }}
-      >
-        <span>
-          I am Paw$n
-          <Typist.Backspace count={2} delay={200} />
-          an.
-        </span>
-        <br />
-        <Typist.Delay ms={1000} />
-        <span>An aspiring </span>
-        <br />
-        <span> Web Developer.</span>
-      </Typist>
+    <div className="board">
+      <span>🔴🟢🟡</span>
+      <div className="name">
+        <Typist
+          avgTypingDelay={50}
+          stdTypingDelay={5}
+          startDelay={2000}
+          cursor={{
+            show: true,
+            hideWhenDone: true,
+          }}
+        >
+          <span>
+            I am Paw$n
+            <Typist.Backspace count={2} delay={200} />
+            an.
+          </span>
+          <br />
+          <Typist.Delay ms={1000} />
+          <span>An aspiring </span>
+          <br />
+          <span> Web Developer.</span>
+        </Typist>
+      </div>
     </div>
   );
 }
